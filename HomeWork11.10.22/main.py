743a018bea421a22db5e4a92680b1857afd29210
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-10-14 17:43:56 +0300

    some changes0.1

--- HomeWork11.10.22/main.py
+++ HomeWork11.10.22/main.py
@@ -10,7 +10,7 @@ def lets_go():
         elif int(num) == 2:
             print("2. Напишите программу, которая принимает на вход число N \n"
                   "и выдает набор произведений чисел от 1 до N.")
-
+            factorial_n()
         elif int(num) == 3:
             print("3. Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.")
 
@@ -24,8 +24,8 @@ def lets_go():
 
         elif int(num) == 0:
             print("\nАнекдот напоследок: \n\n"
-                  "Подскажите, а печатать дома на цветном принтере доллары\n"
-                  "и евро — это еще статья за фальшивомонетничество или уже медаль за импортозамещение?")
+                  "Instagram был заблокирован в России.\n"
+                  "Количество пользователей из Нидерландов увеличилось на 59 млн.\n")
             break
         else:
             print("Enter the correct number, please")
@@ -59,6 +59,7 @@ def count_numbers():
         num = num // 10
     print(f'The sum of the digits of your number is > {int(result)} <')
 
+
 # 2) Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 #
 # *Пример:*
@@ -67,13 +68,14 @@ def count_numbers():
 
 def factorial_n():
     result: int = 1
-    n = enter_number("Enter number:\nN = ")
+    res_list = []
+    n = abs(int(enter_number("Enter positive number:\nN = ")))
     for i in range(n):
         if i <= n:
             i += 1
             result *= i
-            print(result)
-
+            res_list.append(result)
+    print(res_list)
 
 # 3) Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
 #
