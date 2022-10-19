877f00cfe11d0f25de26f157698f6cb3bd8d2259
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-10-19 16:51:50 +0300

    changed1.1.0 homework 3 18.10.22

--- HomeWorkSeminar3/Sem3.py
+++ HomeWorkSeminar3/Sem3.py
@@ -3,12 +3,13 @@ import random
 
 def lets_go():
     while True:
-        print("\n<<<Enter the task number from 1 to 5 to check.>>>\n"
-              "\nFor quit enter 0")
+        print("\n<<<Введите номер задачи от 1 до 5 для проверки.>>>\n"
+              "\nДля выхода введите 0")
         num = enter_number("> ")
         if int(num) == 1:
             print("1. Задайте список из нескольких чисел.\n"
                   " Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.")
+            for_task1()
 
         elif int(num) == 2:
             print("2. Напишите программу, которая найдёт произведение пар чисел списка. \n"
@@ -32,8 +33,11 @@ def lets_go():
             break
 
         else:
-            print("Enter the correct number, please")
+            print("Введите !правильный! номер (от 1 до 5), попробуйте ещё раз...")
 
+def rand_num(start = -100, finish = 100):
+    num = random.randint(start, finish)
+    return num
 
 def enter_number(message):
     number = input(message)
@@ -41,14 +45,15 @@ def enter_number(message):
         float(number)
 
     except ValueError:
-        print("Please enter the number, try again!")
+        print("Введите число, попробуйте ещё раз!")
         return enter_number(message)
     return number
 
 
-def enter_list():
-    user_list = input("Enter your list, separate by space:\n>").split()
+def enter_list(message):
+    user_list = input(message).split()
     return user_list
+
 
 # 1. Задайте список из нескольких чисел. Напишите программу,
 # которая найдёт сумму элементов списка, стоящих на нечётной позиции.
@@ -56,9 +61,28 @@ def enter_list():
 # Пример:
 #
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
-#
-#
-#
+
+def for_task1():
+    user_list = []  # enter_list("Задайте список из нескольких чисел, разделённых пробелами:\n>")
+    rand = rand_num(5, 11)
+    for n in range(rand):
+        user_list.append(rand_num())
+    i = 1
+    result = 0
+    print(f'\nЭто ваш список:\n{user_list}')
+    print("На нечётных позициях находятся элементы: ", sep="", end="")
+    while i < len(user_list):
+        result += int(user_list[i])
+        print(f'-->{user_list[i]}', sep=" ", end=" ")
+        i += 2
+    print(f'\nСуммой элементов стоящих на нечётных позициях вашего списка будет: {result}')
+
+
+for_task1()
+
+
+
+
 # 2. Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 #
@@ -96,3 +120,5 @@ def enter_list():
 # [Негафибоначчи]
 # (Fn = F(n+2)−F(n+1).
 # Они также могут быть определены по формуле F−n = (−1)n+1Fn..)
+
+
