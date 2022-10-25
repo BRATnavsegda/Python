6eb7422d053c18d78a2534b666208efff2b470d3
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-10-25 17:52:24 +0300

    changed1.5.0 homework 4 21.10.22

---
+++ function_file.py
@@ -0,0 +1,40 @@
+import random
+import math
+
+
+def rand_num(start=-100, finish=100):  # получить случайное число
+    num = random.randint(start, finish)
+    return num
+
+
+def enter_number(message):  # ввести цифру с клавиатуры
+    number = input(message)
+    try:
+        float(number)
+
+    except ValueError:
+        print("Введите число, попробуйте ещё раз!")
+        return enter_number(message)
+    return number
+
+
+def enter_list(message):  # ввести список с клавиатуры через пробел
+    user_list = input(message).split()
+    return user_list
+
+
+def write_in(file, input_data):  # записать в файл
+    with open(file, 'w') as data:
+        data.write(input_data)
+
+
+def read_in(file):  # считать из файла
+    with open(file, 'r') as data:
+        r = data.read()
+        return r
+
+
+def sum_dict(dict1, dict2):  # сложение словарей {1: 10, 2: 20} + {1: 10, 2: 20}
+    for e in dict2.keys():   # = {1: 20, 2: 40}
+        dict1[e] = int(dict1.get(e, 0)) + int(dict2[e])
+
