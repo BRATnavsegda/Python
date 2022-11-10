c32c208264d3cff2505a8727a09c4dd7debe46a1
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-11-10 22:38:33 +0300

    continue1.0.0 homework sem6

--- HomeWork08.11.22/Homework0811.py
+++ HomeWork08.11.22/Homework0811.py
@@ -6,11 +6,20 @@ import random
 
 lst = list(random.randint(1, 10) for i in range(1, 20))
 print(f"Исходный список: {lst}")
-# new_lst = []
-# [new_lst.append(i) for i in lst if i not in new_lst]
 
 result = list(filter(lambda n: lst.count(n) == 1, lst))
 
-print(f"Список из неповторяющихся элементов: {result}")
+print(f"Список из неповторяющихся элементов: {result}\n\n")
 
+
+# Задайте список из нескольких чисел. Напишите программу,
+# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+lst = [random.randint(1, 10) for i in range(1, 8)]
+print(f'\nЭто ваш список:\n{lst}')
+
+res = 0
+for i, k in enumerate(lst):
+    res = res + k if i % 2 == 1 else res
+print(f'\nСуммой элементов стоящих на нечётных позициях вашего списка будет: {res}')
 
