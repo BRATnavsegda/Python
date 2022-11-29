1fe323d35ba655b0eed647178ff5bc1aa63f4388
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-11-29 15:30:19 +0300

    homework sem9 end for now

--- HomeWorkSeminar8/add_window.py
+++ HomeWorkSeminar8/add_window.py
@@ -12,9 +12,12 @@ feet_salary = None
 
 def calculate():
     global window
-    data = (feet_fio.get() + ' / ' + feet_tel_number.get() + ' / ' + \
-            feet_job_title.get() + ' / ' + feet_age.get() + ' / ' + feet_salary.get()).replace(',', '.')
-    # a = feet_job_title.get()
+    data = (window.children['frame'].children['frame_fio'].get() +
+            ' / ' + window.children['frame'].children['frame_tel_number'].get() +
+            ' / ' + window.children['frame'].children['frame_job_title'].get() +
+            ' / ' + window.children['frame'].children['frame_age_entry'].get() +
+            ' / ' + window.children['frame'].children['frame_salary'].get()).replace(',', '.')
+    # a = window.children['frame'].children['frame_fio'].get()
     # print(type(a), a)
     ctrl.add_contact_all(data)
     window.destroy()
@@ -28,10 +31,10 @@ def get_value():
     global feet_age
     global feet_salary
 
-    window = Tk()
+    window = Toplevel()
     window.title("Добавление нового служащего")
 
-    mainframe = ttk.Frame(window, padding="100 50 50 50")
+    mainframe = ttk.Frame(window, padding="100 50 50 50", name="frame")
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
     window.columnconfigure(0, weight=1)
     window.rowconfigure(0, weight=1)
@@ -40,27 +43,27 @@ def get_value():
     style.configure("BW.TLabel", foreground="white", background="black")  # создаем шаблон стиля
 
     feet_fio = StringVar()
-    feet_fio_entry = ttk.Entry(mainframe, width=50, textvariable=feet_fio)
+    feet_fio_entry = ttk.Entry(mainframe, width=50, textvariable=feet_fio, name="frame_fio")
     feet_fio_entry.grid(column=2, row=1, sticky=(W, E))
     ttk.Label(mainframe, text='Введите ФИО служащего:').grid(column=1, row=1, sticky=W)
 
     feet_tel_number = StringVar()
-    feet_tel_number_entry = ttk.Entry(mainframe, width=50, textvariable=feet_tel_number)
+    feet_tel_number_entry = ttk.Entry(mainframe, width=50, textvariable=feet_tel_number, name="frame_tel_number")
     feet_tel_number_entry.grid(column=2, row=2, sticky=(W, E))
     ttk.Label(mainframe, text='Введите телефонный номер служащего:').grid(column=1, row=2, sticky=W)
 
     feet_job_title = StringVar()
-    feet_job_title_entry = ttk.Entry(mainframe, width=50, textvariable=feet_job_title)
+    feet_job_title_entry = ttk.Entry(mainframe, width=50, textvariable=feet_job_title, name="frame_job_title")
     feet_job_title_entry.grid(column=2, row=3, sticky=(W, E))
     ttk.Label(mainframe, text='Укажите должность служащего:').grid(column=1, row=3, sticky=W)
 
     feet_age = StringVar()
-    feet_age_entry = ttk.Entry(mainframe, width=50, textvariable=feet_age)
+    feet_age_entry = ttk.Entry(mainframe, width=50, textvariable=feet_age, name="frame_age_entry")
     feet_age_entry.grid(column=2, row=4, sticky=(W, E))
     ttk.Label(mainframe, text='Укажите возраст служащего:').grid(column=1, row=4, sticky=W)
 
     feet_salary = StringVar()
-    feet_salary_entry = ttk.Entry(mainframe, width=50, textvariable=feet_salary)
+    feet_salary_entry = ttk.Entry(mainframe, width=50, textvariable=feet_salary, name="frame_salary")
     feet_salary_entry.grid(column=2, row=5, sticky=(W, E))
     ttk.Label(mainframe, text='Укажите оклад служащего:').grid(column=1, row=5, sticky=W)
 
@@ -80,6 +83,4 @@ def get_value():
 
 
 
-# get_value('text')
-# print(data)
 
