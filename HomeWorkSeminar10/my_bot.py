e6e2a8e07c9cc34163a88c27d09a99368e6a8093
BRATnavsegda <109758944+BRATnavsegda@users.noreply.github.com>
2022-12-01 16:55:27 +0300

    homework sem10 1,1,0

---
+++ HomeWorkSeminar10/my_bot.py
@@ -0,0 +1,57 @@
+import datetime
+
+
+# Выбор формы существительного
+def choose_plural(amount, variants):
+    if amount % 10 == 1 and amount % 100 != 11:
+        variant = 0
+    elif (amount % 10 >= 2) and (amount % 10 <= 4) and (amount % 100 < 10 or amount % 100 >= 20):
+        variant = 1
+    else:
+        variant = 2
+    return '{} {}'.format(amount, variants[variant])
+
+
+def get_your_age(value):
+    res = ''
+    try:
+        # Получаем список и преобразуем нужные данные от пользователя
+
+        datetime_str = value[0] + ' ' + value[1]  # 'Введите дату и время дня рождения в формате ДД.ММ.ГГГГ ЧЧ:ММ\n-->'
+        date_x = datetime.datetime.strptime(datetime_str, '%d.%m.%Y %H:%M')
+        date_now = datetime.datetime.now().replace(second=0, microsecond=0)
+
+        # Программа должна начать выполнять эту часть кода
+        # только если дата рождения меньше текущей даты
+        if date_x < date_now:
+            delta = date_now - date_x
+            # Получаем количество дней, часов и минут,
+            # со дня рождения
+            years = delta.days / 365
+
+            days = delta.days - years * 365
+            hours = delta.seconds // 3600
+            minutes = (delta.seconds - hours * 3600) // 60
+            result = ''
+
+            result = choose_plural(int(years), ('год ', 'года ', 'лет '))  # str(int(years)) + ' и '
+            # Нужная строка формируется
+            if days > 0:
+                result += choose_plural(days, ('день', 'дня', 'дней'))
+                if hours > 0:
+                    result += ' и ' + choose_plural(hours, (' час', 'часа', 'часов'))
+            elif hours > 0:
+                result += choose_plural(hours, ('час', 'часа', 'часов'))
+                if minutes > 0:
+                    result += ' и ' + choose_plural(minutes, ('минута', 'минуты', 'минут'))
+            else:
+                result += choose_plural(minutes, ('минута', 'минуты', 'минут'))
+            res = 'Сейчас вам ' + result
+        else:
+            res = 'Ошибка! \nУказанная вами дата из будущего...'
+    except ValueError:
+        # Если пользователь введет дату не по формату,
+        # отработает этот обработчик исключения
+        res = 'Ошибка! \nФормат ввода - ДД.ММ.ГГГГ ЧЧ:ММ'
+
+    return res
---
+++ HomeWorkSeminar10/tele_bot.py
@@ -0,0 +1,88 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging
+import my_bot
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, filename="my_log.log", filemode="a")
+
+
+TOKEN = '5822831148:AAElou3LjtIhNCQFA99G5jPmwNrsLp_-Ou4'
+# получаем экземпляр `Updater`
+updater = Updater(token=TOKEN, use_context=True)
+# получаем экземпляр `Dispatcher`
+dispatcher = updater.dispatcher
+
+# Обратите внимание, что из обработчика в функцию
+# передаются экземпляры `update` и `context`
+
+
+def start(update, context):
+    # `bot.send_message` это метод Telegram API
+    # `update.effective_chat.id` - определяем `id` чата,
+    # откуда прилетело сообщение
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Я бот, который может посчитать\nсколько времени ты взираешь на этот Мир!\n\n"
+                                  "Для этого введите команду:\n /data <Ваш день рождения>\n"
+                                  "Пример:\n"
+                                  "/data 01.01.1999 01:01 ")
+# импортируем обработчик CommandHandler,
+# который фильтрует сообщения с командами
+
+# говорим обработчику, если увидишь команду `/start`,
+# то вызови функцию `start()`
+
+
+start_handler = CommandHandler('start', start)
+
+# добавляем этот обработчик в `dispatcher`
+dispatcher.add_handler(start_handler)
+
+# функция обратного вызова
+def echo(update, context):
+    # добавим в начало полученного сообщения строку 'ECHO: '
+    text = 'Неопознанная команда...\n' \
+           'Доступные команды:\n' \
+           '/start\n' \
+           '/data число.месяц.год час:минуты'  # + update.message.text (вернуть пользователю его сообщение)
+    # `update.effective_chat.id` - определяем `id` чата,
+    # откуда прилетело сообщение
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=text)
+# говорим обработчику `MessageHandler`, если увидишь текстовое
+# сообщение (фильтр `Filters.text`)  и это будет не команда
+# (фильтр ~Filters.command), то вызови функцию `echo()`
+
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+# регистрируем обработчик `echo_handler` в экземпляре `dispatcher`
+dispatcher.add_handler(echo_handler)
+
+
+def data(update, context):
+    # если аргументы присутствуют
+    logging.info(f'ID {update.effective_chat.id} {context.args} ')
+    if context.args:
+        # передаем аргументы в свою функцию
+        text_diff = my_bot.get_your_age(context.args)
+        # `update.effective_chat.id` - определяем `id` чата,
+        # откуда прилетело сообщение
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=text_diff)
+    else:
+        # если в команде не указан аргумент
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='В команде не указана дата')
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Пример: /data число.месяц.год час:минуты')
+
+
+# обработчик команды '/data'
+caps_handler = CommandHandler('data', data)
+# регистрируем обработчик в диспетчере
+dispatcher.add_handler(caps_handler)
+
+# говорим экземпляру `Updater`,
+# слушай сервера Telegram.
+updater.start_polling()
